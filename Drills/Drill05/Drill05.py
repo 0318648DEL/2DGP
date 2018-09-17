0261c3f2ae5da7df8b49457eac3551f78a836510
재범 서 <gleldzm3212@kpu.ac.kr>
2018-09-17 21:43:43 +0900

    캐릭터 좌우 움직임 함수와 동작 결정 함수 수정
    내용 수정함

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -6,13 +6,38 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 # fill here
-def move_left():
+def move_left_up(x1, y1,x2,y2):
+    move_x=abs(x1-x2)/32
+    move_y=abs(y1-y2)/32
+    curr_x=x1
+    curr_y=y1
+    frame=0
+    while curr_x>x2 and curr_y<y2:
+        clear_canvas_now()
+        grass.draw(400,30)
+        character.clip_draw(100*frame,0,100,100,curr_x,curr_y)
+        update_canvas()
+        frame=(frame+1)%8
+        curr_x-=move_x
+        curr_y+=move_y
+        delay(0.05)
     pass
 
-def move_right():
+def move_left_down():
+    pass
+
+def move_right_up():
+    pass
+
+def move_right_down():
     pass
 
 def char_move():
+    count=0
+    data=[[203,535],[132,243],[535,470],[477,203],[715,136],[316,225],[510,92],[692,518],[682,336],[712,349]]
+    while count<9:
+        if data[count][0]>data[count+1][0] and data[count][1]<data[count+1][1]:
+            move_left_up(data[count][0],data[count][1],data[count+1][0],data[count+1][1])
     pass
 
 while True:
