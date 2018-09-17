3b75801121a71f97578d2a767b065bab997dae98
재범 서 <gleldzm3212@kpu.ac.kr>
2018-09-17 21:25:56 +0900

    함수 선언
    캐릭터 이동에 필요한 함수 선언

---
+++ Drills/Drill05/Drill05.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+# fill here
+def move_left():
+    pass
+
+def move_right():
+    pass
+
+def char_move():
+    pass
+
+while True:
+    char_move()
+
+close_canvas()
