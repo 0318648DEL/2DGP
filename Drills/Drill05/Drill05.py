f1f56b7e6b4d42b0f00cf3eef728c32e7e3d428d
재범 서 <gleldzm3212@kpu.ac.kr>
2018-09-17 21:49:41 +0900

    좌하 함수 완료
    함수 내용 입력 및 이동 결정 함수 수정

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -21,9 +21,22 @@ def move_left_up(x1, y1,x2,y2):
         curr_x-=move_x
         curr_y+=move_y
         delay(0.05)
-    pass
 
-def move_left_down():
+def move_left_down(x1, y1,x2,y2):
+    move_x = abs(x1 - x2) / 32
+    move_y = abs(y1 - y2) / 32
+    curr_x = x1
+    curr_y = y1
+    frame = 0
+    while curr_x > x2 and curr_y > y2:
+        clear_canvas_now()
+        grass.draw(400, 30)
+        character.clip_draw(100 * frame, 0, 100, 100, curr_x, curr_y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        curr_x -= move_x
+        curr_y -= move_y
+        delay(0.05)
     pass
 
 def move_right_up():
@@ -38,7 +51,9 @@ def char_move():
     while count<9:
         if data[count][0]>data[count+1][0] and data[count][1]<data[count+1][1]:
             move_left_up(data[count][0],data[count][1],data[count+1][0],data[count+1][1])
-    pass
+        elif data[count][0]>data[count+1][0] and data[count][1]>data[count+1][1]:
+            move_left_down(data[count][0],data[count][1],data[count+1][0],data[count+1][1])
+
 
 while True:
     char_move()
