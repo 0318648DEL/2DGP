a9b955bb1b6a968f259f61985ea08857e24f6e9a
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-04 16:54:05 +0900

    캐릭터 방향 설정
    좌상 이동

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -10,5 +10,20 @@ character = load_image('animation_sheet.png')
 size = 10
 points = [(random.randint(0, KPU_WIDTH), random.randint(0, KPU_HEIGHT)) for i in range(size)]
 
+def move_left_up(x1, y1,x2,y2):
+    move_x=abs(x1-x2)/32
+    move_y=abs(y1-y2)/32
+    curr_x=x1
+    curr_y=y1
+    frame=0
+    while curr_x>x2 and curr_y<y2:
+        clear_canvas_now()
+        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(100*frame,0,100,100,curr_x,curr_y)
+        update_canvas()
+        frame=(frame+1)%8
+        curr_x-=move_x
+        curr_y+=move_y
+        delay(0.05)
 
 close_canvas()
