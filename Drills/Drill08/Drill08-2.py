2dd1cd34caa74628483f2e537b675946612e5da5
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-04 16:53:17 +0900

    10개 점 무작위로 찍기
    점 변수 선언

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -7,4 +7,8 @@ open_canvas(KPU_WIDTH,KPU_HEIGHT)
 background = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+size = 10
+points = [(random.randint(0, KPU_WIDTH), random.randint(0, KPU_HEIGHT)) for i in range(size)]
+
+
 close_canvas()
