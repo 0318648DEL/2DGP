2e28c3e6c0eb31269dc797dba51bc08e6ad8e722
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-04 17:17:09 +0900

    부드러운 이동
    4번째에서 5번째까지
    5번째에서 6번째까지
    6번째에서 7번째까지
    7번째에서 8번째까지

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -122,5 +122,67 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         elif p3[0]<p4[0] and p3[1]>p4[1]:
             move_right_down(p3[0],p3[1],p4[0],p4[1])
 
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p5[0] + (t ** 3 - t ** 2) * p6[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p6[1]) / 2
+        if p4[0]>p5[0] and p4[1]<p5[1]:
+            move_left_up(p4[0],p4[1],p5[0],p5[1])
+        elif p4[0]>p5[0] and p4[1]>p5[1]:
+            move_left_down(p4[0],p4[1],p5[0],p5[1])
+        elif p4[0]<p5[0] and p4[1]<p5[1]:
+            move_right_up(p4[0],p4[1],p5[0],p5[1])
+        elif p4[0]<p5[0] and p4[1]>p5[1]:
+            move_right_down(p4[0],p4[1],p5[0],p5[1])
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p6[0] + (t ** 3 - t ** 2) * p7[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
+        if p5[0]>p6[0] and p5[1]<p6[1]:
+            move_left_up(p5[0],p5[1],p6[0],p6[1])
+        elif p5[0]>p6[0] and p5[1]>p6[1]:
+            move_left_down(p5[0],p5[1],p6[0],p6[1])
+        elif p5[0]<p6[0] and p5[1]<p6[1]:
+            move_right_up(p5[0],p5[1],p6[0],p6[1])
+        elif p5[0]<p6[0] and p5[1]>p6[1]:
+            move_right_down(p5[0],p5[1],p6[0],p6[1])
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p5[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p7[0] + (t ** 3 - t ** 2) * p8[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p5[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p7[1] + (t ** 3 - t ** 2) * p8[1]) / 2
+        if p6[0]>p7[0] and p6[1]<p7[1]:
+            move_left_up(p6[0],p6[1],p7[0],p7[1])
+        elif p6[0]>p7[0] and p6[1]>p7[1]:
+            move_left_down(p6[0],p6[1],p7[0],p7[1])
+        elif p6[0]<p7[0] and p6[1]<p7[1]:
+            move_right_up(p6[0],p6[1],p7[0],p7[1])
+        elif p6[0]<p7[0] and p6[1]>p7[1]:
+            move_right_down(p6[0],p6[1],p7[0],p7[1])
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p6[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p8[0] + (t ** 3 - t ** 2) * p9[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p6[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p8[1] + (t ** 3 - t ** 2) * p9[1]) / 2
+        if p7[0]>p8[0] and p7[1]<p8[1]:
+            move_left_up(p7[0],p7[1],p8[0],p8[1])
+        elif p7[0]>p8[0] and p7[1]>p8[1]:
+            move_left_down(p7[0],p7[1],p8[0],p8[1])
+        elif p7[0]<p8[0] and p7[1]<p8[1]:
+            move_right_up(p7[0],p7[1],p8[0],p8[1])
+        elif p7[0]<p8[0] and p7[1]>p8[1]:
+            move_right_down(p7[0],p7[1],p8[0],p8[1])
+
+
+
 
 close_canvas()
