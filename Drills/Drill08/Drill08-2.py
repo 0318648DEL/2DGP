524f05ca81d99b684893ac0250199f9ddd79797b
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-04 17:56:38 +0900

    실습 08-2 완료
    총 커밋 횟수 11번
    https://youtu.be/HZfSrpgDqPw

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -8,73 +8,46 @@ background = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 size = 10
+frame = 0
 points = [(random.randint(0, KPU_WIDTH), random.randint(0, KPU_HEIGHT)) for i in range(size)]
 
-def move_left_up(x1, y1,x2,y2):
-    move_x=abs(x1-x2)/32
-    move_y=abs(y1-y2)/32
+def move_left_up(x1, y1,f):
     curr_x=x1
     curr_y=y1
-    frame=0
-    while curr_x>x2 and curr_y<y2:
-        clear_canvas_now()
-        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        character.clip_draw(100*frame,0,100,100,curr_x,curr_y)
-        update_canvas()
-        frame=(frame+1)%8
-        curr_x-=move_x
-        curr_y+=move_y
-        delay(0.05)
+    clear_canvas_now()
+    background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(100*f,0,100,100,curr_x,curr_y)
+    update_canvas()
+    delay(0.05)
 
-def move_left_down(x1, y1,x2,y2):
-    move_x = abs(x1 - x2) / 32
-    move_y = abs(y1 - y2) / 32
+def move_left_down(x1, y1,f):
     curr_x = x1
     curr_y = y1
-    frame = 0
-    while curr_x > x2 and curr_y > y2:
-        clear_canvas_now()
-        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        character.clip_draw(100 * frame, 0, 100, 100, curr_x, curr_y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        curr_x -= move_x
-        curr_y -= move_y
-        delay(0.05)
+    clear_canvas_now()
+    background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(100 * f, 0, 100, 100, curr_x, curr_y)
+    update_canvas()
+    delay(0.05)
 
-def move_right_up(x1, y1,x2,y2):
-    move_x = abs(x1 - x2) / 32
-    move_y = abs(y1 - y2) / 32
+def move_right_up(x1, y1,f):
     curr_x = x1
     curr_y = y1
-    frame = 0
-    while curr_x < x2 and curr_y < y2:
-        clear_canvas_now()
-        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        character.clip_draw(100 * frame, 100, 100, 100, curr_x, curr_y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        curr_x += move_x
-        curr_y += move_y
-        delay(0.05)
+    clear_canvas_now()
+    background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(100 * f, 100, 100, 100, curr_x, curr_y)
+    update_canvas()
+    delay(0.05)
 
-def move_right_down(x1, y1,x2,y2):
-    move_x = abs(x1 - x2) / 32
-    move_y = abs(y1 - y2) / 32
+def move_right_down(x1, y1,f):
     curr_x = x1
     curr_y = y1
-    frame = 0
-    while curr_x < x2 and curr_y > y2:
-        clear_canvas_now()
-        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        character.clip_draw(100 * frame, 100, 100, 100, curr_x, curr_y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        curr_x += move_x
-        curr_y -= move_y
-        delay(0.05)
+    clear_canvas_now()
+    background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(100 * f, 100, 100, 100, curr_x, curr_y)
+    update_canvas()
+    delay(0.05)
 
-def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
+def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,f):
 
     # draw p1-p2
     for i in range(0, 100, 2):
@@ -84,13 +57,14 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p10[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
         if p1[0]>p2[0] and p1[1]<p2[1]:
-            move_left_up(p1[0],p1[1],p2[0],p2[1])
+            move_left_up(x,y,f)
         elif p1[0]>p2[0] and p1[1]>p2[1]:
-            move_left_down(p1[0],p1[1],p2[0],p2[1])
+            move_left_down(x,y,f)
         elif p1[0]<p2[0] and p1[1]<p2[1]:
-            move_right_up(p1[0],p1[1],p2[0],p2[1])
+            move_right_up(x,y,f)
         elif p1[0]<p2[0] and p1[1]>p2[1]:
-            move_right_down(p1[0],p1[1],p2[0],p2[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -99,13 +73,14 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
         if p2[0]>p3[0] and p2[1]<p3[1]:
-            move_left_up(p2[0],p2[1],p3[0],p3[1])
+            move_left_up(x,y,f)
         elif p2[0]>p3[0] and p2[1]>p3[1]:
-            move_left_down(p2[0],p2[1],p3[0],p3[1])
+            move_left_down(x,y,f)
         elif p2[0]<p3[0] and p2[1]<p3[1]:
-            move_right_up(p2[0],p2[1],p3[0],p3[1])
+            move_right_up(x,y,f)
         elif p2[0]<p3[0] and p2[1]>p3[1]:
-            move_right_down(p2[0],p2[1],p3[0],p3[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -114,13 +89,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
         if p3[0]>p4[0] and p3[1]<p4[1]:
-            move_left_up(p3[0],p3[1],p4[0],p4[1])
+            move_left_up(x,y,f)
         elif p3[0]>p4[0] and p3[1]>p4[1]:
-            move_left_down(p3[0],p3[1],p4[0],p4[1])
+            move_left_down(x,y,f)
         elif p3[0]<p4[0] and p3[1]<p4[1]:
-            move_right_up(p3[0],p3[1],p4[0],p4[1])
+            move_right_up(x,y,f)
         elif p3[0]<p4[0] and p3[1]>p4[1]:
-            move_right_down(p3[0],p3[1],p4[0],p4[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -129,13 +106,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p6[1]) / 2
         if p4[0]>p5[0] and p4[1]<p5[1]:
-            move_left_up(p4[0],p4[1],p5[0],p5[1])
+            move_left_up(x,y,f)
         elif p4[0]>p5[0] and p4[1]>p5[1]:
-            move_left_down(p4[0],p4[1],p5[0],p5[1])
+            move_left_down(x,y,f)
         elif p4[0]<p5[0] and p4[1]<p5[1]:
-            move_right_up(p4[0],p4[1],p5[0],p5[1])
+            move_right_up(x,y,f)
         elif p4[0]<p5[0] and p4[1]>p5[1]:
-            move_right_down(p4[0],p4[1],p5[0],p5[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -144,13 +123,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
         if p5[0]>p6[0] and p5[1]<p6[1]:
-            move_left_up(p5[0],p5[1],p6[0],p6[1])
+            move_left_up(x,y,f)
         elif p5[0]>p6[0] and p5[1]>p6[1]:
-            move_left_down(p5[0],p5[1],p6[0],p6[1])
+            move_left_down(x,y,f)
         elif p5[0]<p6[0] and p5[1]<p6[1]:
-            move_right_up(p5[0],p5[1],p6[0],p6[1])
+            move_right_up(x,y,f)
         elif p5[0]<p6[0] and p5[1]>p6[1]:
-            move_right_down(p5[0],p5[1],p6[0],p6[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -159,13 +140,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p5[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p7[1] + (t ** 3 - t ** 2) * p8[1]) / 2
         if p6[0]>p7[0] and p6[1]<p7[1]:
-            move_left_up(p6[0],p6[1],p7[0],p7[1])
+            move_left_up(x,y,f)
         elif p6[0]>p7[0] and p6[1]>p7[1]:
-            move_left_down(p6[0],p6[1],p7[0],p7[1])
+            move_left_down(x,y,f)
         elif p6[0]<p7[0] and p6[1]<p7[1]:
-            move_right_up(p6[0],p6[1],p7[0],p7[1])
+            move_right_up(x,y,f)
         elif p6[0]<p7[0] and p6[1]>p7[1]:
-            move_right_down(p6[0],p6[1],p7[0],p7[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -174,13 +157,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p6[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p8[1] + (t ** 3 - t ** 2) * p9[1]) / 2
         if p7[0]>p8[0] and p7[1]<p8[1]:
-            move_left_up(p7[0],p7[1],p8[0],p8[1])
+            move_left_up(x,y,f)
         elif p7[0]>p8[0] and p7[1]>p8[1]:
-            move_left_down(p7[0],p7[1],p8[0],p8[1])
+            move_left_down(x,y,f)
         elif p7[0]<p8[0] and p7[1]<p8[1]:
-            move_right_up(p7[0],p7[1],p8[0],p8[1])
+            move_right_up(x,y,f)
         elif p7[0]<p8[0] and p7[1]>p8[1]:
-            move_right_down(p7[0],p7[1],p8[0],p8[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -189,13 +174,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p7[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p9[1] + (t ** 3 - t ** 2) * p10[1]) / 2
         if p8[0]>p9[0] and p8[1]<p9[1]:
-            move_left_up(p8[0],p8[1],p9[0],p9[1])
+            move_left_up(x,y,f)
         elif p8[0]>p9[0] and p8[1]>p9[1]:
-            move_left_down(p8[0],p8[1],p9[0],p9[1])
+            move_left_down(x,y,f)
         elif p8[0]<p9[0] and p8[1]<p9[1]:
-            move_right_up(p8[0],p8[1],p9[0],p9[1])
+            move_right_up(x,y,f)
         elif p8[0]<p9[0] and p8[1]>p9[1]:
-            move_right_down(p8[0],p8[1],p9[0],p9[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -204,13 +191,15 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p8[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p10[1] + (t ** 3 - t ** 2) * p1[1]) / 2
         if p9[0]>p10[0] and p9[1]<p10[1]:
-            move_left_up(p9[0],p9[1],p10[0],p10[1])
+            move_left_up(x,y,f)
         elif p9[0]>p10[0] and p9[1]>p10[1]:
-            move_left_down(p9[0],p9[1],p10[0],p10[1])
+            move_left_down(x,y,f)
         elif p9[0]<p10[0] and p9[1]<p10[1]:
-            move_right_up(p9[0],p9[1],p10[0],p10[1])
+            move_right_up(x,y,f)
         elif p9[0]<p10[0] and p9[1]>p10[1]:
-            move_right_down(p9[0],p9[1],p10[0],p10[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
+
 
     for i in range(0, 100, 2):
         t = i / 100
@@ -219,16 +208,18 @@ def draw_curve_10p_connect(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10):
         y = ((-t ** 3 + 2 * t ** 2 - t) * p9[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
         if p10[0]>p1[0] and p10[1]<p1[1]:
-            move_left_up(p10[0],p10[1],p1[0],p1[1])
+            move_left_up(x,y,f)
         elif p10[0]>p1[0] and p10[1]>p1[1]:
-            move_left_down(p10[0],p10[1],p1[0],p1[1])
+            move_left_down(x,y,f)
         elif p10[0]<p1[0] and p10[1]<p1[1]:
-            move_right_up(p10[0],p10[1],p1[0],p1[1])
+            move_right_up(x,y,f)
         elif p10[0]<p1[0] and p10[1]>p1[1]:
-            move_right_down(p10[0],p10[1],p1[0],p1[1])
+            move_right_down(x,y,f)
+        f = (f + 1) % 8
 
 
-
-
+while True:
+    draw_curve_10p_connect(points[0], points[1], points[2], points[3], points[4], points[5], points[6], points[7],
+                           points[8], points[9], frame)
 
 close_canvas()
