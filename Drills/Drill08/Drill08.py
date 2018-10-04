2860cb1ebf119e8fc5599f9da39697a0188945f3
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-04 16:43:54 +0900

    함수 내용 입력
    3번째 점에서 4번째 점까지

--- Drills/Drill08/Drill08.py
+++ Drills/Drill08/Drill08.py
@@ -72,6 +72,16 @@ def draw_curve_4p_connect(p1,p2,p3,p4):
         draw_point((x, y))
     draw_point(p3)
 
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        draw_point((x, y))
+    draw_point(p4)
+
+
 prepare_turtle_canvas()
 
 
