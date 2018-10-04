58f79d996110ef7dc2e16a7ef60fda0f340ac8d0
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-04 15:55:32 +0900

    캐릭터 반복 이동 완성
    총 커밋 횟수 2번

--- Drills/Drill07/Drill07.py
+++ Drills/Drill07/Drill07.py
@@ -2,25 +2,98 @@ import random
 from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
-kpu_ground = load_image('KPU_GROUND.png')
+
+background = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-    # fill here
+size = 20
+points = [(random.randint(0, KPU_WIDTH), random.randint(0, KPU_HEIGHT)) for i in range(size)]
 
+def move_left_up(x1, y1,x2,y2):
+    move_x=abs(x1-x2)/32
+    move_y=abs(y1-y2)/32
+    curr_x=x1
+    curr_y=y1
+    frame=0
+    while curr_x>x2 and curr_y<y2:
+        clear_canvas_now()
+        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(100*frame,0,100,100,curr_x,curr_y)
+        update_canvas()
+        frame=(frame+1)%8
+        curr_x-=move_x
+        curr_y+=move_y
+        delay(0.05)
 
-def character_moving(p1, p2):
-    for i in range(0, 100 + 1, 2):
-        t = i / 100
-        x = (1 - t) * p1[0] + t * p2[0]
-        y = (1 - t) * p1[1] + t * p2[1]
+def move_left_down(x1, y1,x2,y2):
+    move_x = abs(x1 - x2) / 32
+    move_y = abs(y1 - y2) / 32
+    curr_x = x1
+    curr_y = y1
+    frame = 0
+    while curr_x > x2 and curr_y > y2:
+        clear_canvas_now()
+        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(100 * frame, 0, 100, 100, curr_x, curr_y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        curr_x -= move_x
+        curr_y -= move_y
+        delay(0.05)
 
-    # fill here
+def move_right_up(x1, y1,x2,y2):
+    move_x = abs(x1 - x2) / 32
+    move_y = abs(y1 - y2) / 32
+    curr_x = x1
+    curr_y = y1
+    frame = 0
+    while curr_x < x2 and curr_y < y2:
+        clear_canvas_now()
+        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(100 * frame, 100, 100, 100, curr_x, curr_y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        curr_x += move_x
+        curr_y += move_y
+        delay(0.05)
 
+def move_right_down(x1, y1,x2,y2):
+    move_x = abs(x1 - x2) / 32
+    move_y = abs(y1 - y2) / 32
+    curr_x = x1
+    curr_y = y1
+    frame = 0
+    while curr_x < x2 and curr_y > y2:
+        clear_canvas_now()
+        background.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(100 * frame, 100, 100, 100, curr_x, curr_y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        curr_x += move_x
+        curr_y -= move_y
+        delay(0.05)
 
-size = 6
-points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+def char_move():
+    count=0
+    while count<size:
+        if points[count][0]>points[(count+1)%size][0] and points[count][1]<points[(count+1)%size][1]:
+            move_left_up(points[count][0],points[count][1],points[(count+1)%size][0],points[(count+1)%size][1])
+        elif points[count][0]>points[(count+1)%size][0] and points[count][1]>points[(count+1)%size][1]:
+            move_left_down(points[count][0],points[count][1],points[(count+1)%size][0],points[(count+1)%size][1])
+        elif points[count][0]<points[(count+1)%size][0] and points[count][1]<points[(count+1)%size][1]:
+            move_right_up(points[count][0],points[count][1],points[(count+1)%size][0],points[(count+1)%size][1])
+        elif points[count][0]<points[(count+1)%size][0] and points[count][1]>points[(count+1)%size][1]:
+            move_right_down(points[count][0],points[count][1],points[(count+1)%size][0],points[(count+1)%size][1])
+        count += 1
 
+while True:
+    char_move()
 
 close_canvas()
+
+
+
+
+
+
