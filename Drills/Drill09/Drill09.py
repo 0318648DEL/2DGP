51eaf669bfec15b8e99217b49494dd62a7783de7
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-08 16:55:28 +0900

    큰공 객체 선언
    큰공 클래스 생성

--- Drills/Drill09/Drill09.py
+++ Drills/Drill09/Drill09.py
@@ -12,3 +12,15 @@ class Ball21:
 
     def draw(self):
         self.image.draw(self.x,self.y)
+
+class Ball41:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), random.randint(100, 500)
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if (self.y >= 80):
+            self.y -= 5
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
