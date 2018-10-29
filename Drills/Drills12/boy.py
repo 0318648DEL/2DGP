3c7dcbc662eedb3122dfc4d4e02e0d0faf55a8d5
재범 서 <gleldzm3212@kpu.ac.kr>
2018-10-29 18:09:21 +0900

    반투명한 고스트 생성
    반투명한 고스트를 만들어냈다
    아직 원운동 하지 않음

--- Drills/Drills12/boy.py
+++ Drills/Drills12/boy.py
@@ -121,10 +121,15 @@ class SleepState:
     @staticmethod
     def draw(boy):
         if boy.dir == 1:
+            boy.image.opacify(1)
             boy.image.clip_composite_draw(int(boy.frame) * 100, 300, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+            boy.image.opacify(0.2)
+            boy.image.clip_draw(int(boy.frame) * 100, 300, 100, 100, boy.x, boy.y)
         else:
+            boy.image.opacify(1)
             boy.image.clip_composite_draw(int(boy.frame) * 100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
-
+            boy.image.opacify(0.2)
+            boy.image.clip_draw(int(boy.frame) * 100, 300, 100, 100, boy.x, boy.y)
 
 
 
